eee453568eb34176ae034281cdab8967e7f472d6
sushmita08 <55182715+sushmita08@users.noreply.github.com>
2025-02-20 12:07:55 +0000

    numbers and math

---
+++ .devcontainer/basics/03_numbers.js
@@ -0,0 +1,21 @@
+const num1 = 400
+
+const num2 =  new Number(100)
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.3));
+console.log(Math.ceil(4.3));
+console.log(Math.floor(4.9));
+
+console.log(Math.random())// will give values between 0-1
+
+console.log(Math.floor(Math.random()*10)+1)
+
+const min = 20
+const max = 40
+console.log(Math.floor((Math.random() * (max - min + 1)))+min)
+
+
+
+
