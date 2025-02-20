b4cebb207c65422b582e30e04f732dcde9983723
sushmita08 <55182715+sushmita08@users.noreply.github.com>
2025-02-20 07:08:21 +0000

    01_basics code

---
+++ .devcontainer/Projects/01_basics.js
@@ -0,0 +1,7 @@
+const accountId = "1234"
+let emailId = "sush@gmail.com"
+var accountType = "saving"
+accontName = "sushmita"
+let accountState;
+
+console.table([accountId,emailId,accountType,accontName,accountState])
